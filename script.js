4286ca725ad468252e41e7a35ea53900ebf47358
theantidoak <tcdoak@gmail.com>
2023-05-20 16:51:16 +0200

    finish calls

--- script.js
+++ script.js
@@ -110,10 +110,166 @@ class Tree {
     return root;
   }
 
-  levelOrder(fn) {
-    
+  levelOrder(root) {
+    if (root == null) {
+      return root;
+    }
+
+    let q = [];
+    let valueArray = [root.data];
+    let counter = 0;
+    q.push(root);
+
+    while (q.length > counter) {
+      let child = q[counter];
+
+      if (child.left !== null) {
+        q.push(child.left);
+        valueArray.push(child.left.data);
+      }
+      if (child.right !== null) {
+        q.push(child.right);
+        valueArray.push(child.right.data);
+      }
+
+      counter++;
+    }
+
+    return valueArray;
   }
-  
+
+  inorder(root, q=[], valueArray=[]) {
+    if (root == null) {
+      return valueArray;
+    }
+
+    q.push(root);
+
+    if (root.left !== null) {
+      this.inorder(root.left, q, valueArray);
+    }
+
+    valueArray.push(root.data);
+
+    if (root.right !== null) {
+      this.inorder(root.right, q, valueArray);
+    }
+
+    return valueArray;
+  }
+
+  preorder(root, q=[], valueArray=[root.data]) {
+    if (root == null) {
+      return valueArray;
+    }
+
+    q.push(root);
+
+    if (root.left !== null) {
+      valueArray.push(root.left.data);
+      this.preorder(root.left, q, valueArray);
+    }
+    if (root.right !== null) {
+      valueArray.push(root.right.data);
+      this.preorder(root.right, q, valueArray);
+    }
+
+    return valueArray;
+  }
+
+  postorder(root, q=[], valueArray=[]) {
+    if (root == null) {
+      return valueArray;
+    }
+
+    q.push(root);
+
+    if (root.left !== null) {
+      this.postorder(root.left, q, valueArray);
+    }
+    
+    if (root.right !== null) {
+      this.postorder(root.right, q, valueArray);
+    }
+
+    valueArray.push(root.data);
+
+    return valueArray;
+  }
+
+  height(node, counterL=0, counterR=0) {
+
+    if (node == null) {
+      return counterL;
+    }
+
+    if (node.left !== null) {
+      counterL++;
+      counterL = this.height(node.left, counterL, counterR);
+    }
+
+    if (node.right !== null) {
+      counterR++;
+      counterR = this.height(node.right, counterL, counterR);
+    }
+
+    return counterR > counterL ? counterR : counterL;
+  }
+
+  depth(node) {
+    const nodeHeight = this.height(node);
+    const rootHeight = this.height(this.root);
+
+    return rootHeight - nodeHeight;
+  }
+
+  isBalanced(node) {
+    if (node == null) {
+      return;
+    }
+
+    if (node.left !== null) {
+      this.isBalanced(node.left);
+      
+      const leftHeight = this.height(node.left);
+      const rightHeight = node.right == null ? null : this.height(node.right);
+      const diff = rightHeight ? leftHeight - rightHeight : null;
+
+      if ((!rightHeight && this.depth(node.left) > 1) || (diff && diff > 1 && diff < -1)) {
+        return false;
+      }
+    }
+    
+    if (node.right !== null) {
+      this.isBalanced(node.left);
+      
+      const rightHeight = this.height(node.right);
+      const leftHeight = node.left == null ? null : this.height(node.left);
+      const diff = leftHeight ? rightHeight - leftHeight : null;
+
+      if ((!leftHeight && this.depth(node.right) > 1) || (diff && diff > 1 && diff < -1)) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  rebalance(node) {
+    if (node == null) {
+      return;
+    }
+
+    const balanced = isBalanced(node);
+
+    if (!balanced) {
+      const newSortedArray = this._removeDuplicates(this.preorder(node));
+      this.root = this.buildTree(newSortedArray, 0, newSortedArray.length - 1);
+      this.buildTree(newSortedArray, 0, newSortedArray.length - 1);
+    }
+
+    return node;
+  }
 }
 
 const test = new Tree(arr);
@@ -122,6 +278,11 @@ test.insertNode(40);
 test.deleteNode(9);
 
 prettyPrint(test.root);
-prettyPrint(test.find(67));
+// prettyPrint(test.find(67));
 
-
+// console.log(test.inorder(test.root));
+// console.log(test.preorder(test.root));
+// console.log(test.postorder(test.root));
+// console.log(test.height(test.root));
+// console.log(test.depth(test.root.left.left.right));
+console.log(test.isBalanced(test.root))
